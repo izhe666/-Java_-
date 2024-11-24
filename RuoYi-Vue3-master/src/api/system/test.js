64850a0fafe5124d7eda6ae97673f0700bd5adb6
izhe <2531167601@qq.com>
2024-11-25 01:12:04 +0800

    my后端Springboot
    后端

--- RuoYi-Vue3-master/src/api/system/test.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function testController(data) {
-  return request({
-    url: 'http://localhost:8080/test',
-    method: 'post',
-    data: data
-  })
-}
